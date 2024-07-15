015be3f2bdecefe911060ff0f2bea08a80b7d849
Riche <maksoricheisaac@gmail.com>
2024-07-15 12:50:15 +0100

    first commit

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -6,7 +6,7 @@ const Jumbotron = () => {
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative">
        
         <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Investir dans le potentiel du monde</h1>
-        <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">Ici, nous nous concentrons sur les marchés où la technologie, l'innovation et le capital peuvent débloquer une valeur à long terme et stimuler la croissance économique.</p>
+        <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">Ici, nous nous concentrons sur les marchés où la technologie,  innovation et le capital peuvent débloquer une valeur à long terme et stimuler la croissance économique.</p>
 
     </div>
     <div class="bg-gradient-to-b from-blue-50 to-transparent dark:from-blue-900 w-full h-full  top-16 left-0 z-0"></div>
